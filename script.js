2281e94267acf2ed9520530753cf58ecd218702a
Vaishnavi  Gomkale <109347851+gomkalevaishnavi@users.noreply.github.com>
2023-09-10 08:54:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 // your code here
+ // Get references to form elements and the h3 element
+        const urlForm = document.getElementById('urlForm');
+        const nameInput = document.getElementById('name');
+        const yearInput = document.getElementById('year');
+        const urlElement = document.getElementById('url');
+
+        // Add an event listener to the form submit
+        urlForm.addEventListener('submit', function (event) {
+            event.preventDefault(); // Prevent the form from submitting
+
+            // Get the input values
+            const name = nameInput.value;
+            const year = yearInput.value;
+
+            // Construct the query string
+            const queryString = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+
+            // Update the h3 element with the generated URL
+            urlElement.textContent = ` https://localhost:8080/${queryString}`;
+        });
